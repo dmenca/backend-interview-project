17903714970a640b7111008b6a7ca16d253aa8d0
caoan <caoan@sensetime.com>
2023-09-26 14:30:32 +0800

    add proxy test

--- java-basic/src/main/java/com/demnca/java/basic/proxy/statics/DebugInvocationHandler.java
+++ java-basic/src/main/java/com/demnca/java/basic/proxy/statics/DebugInvocationHandler.java
@@ -1,12 +1,37 @@
 package com.demnca.java.basic.proxy.statics;
 
+import com.demnca.java.basic.proxy.vo.Animal;
+import com.demnca.java.basic.proxy.vo.Cat;
+import jdk.nashorn.internal.runtime.Debug;
+
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 public class DebugInvocationHandler implements InvocationHandler {
 
+    private final Object target;
+
+    public DebugInvocationHandler(Object target){
+        this.target = target;
+    }
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        return null;
+        System.out.println("before method " + method.getName());
+        Object result = method.invoke(target, args);
+        System.out.println("after method " + method.getName());
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Cat realObject = new Cat();
+        // 创建 InvocationHandler 对象并关联被代理对象
+        DebugInvocationHandler debugInvocationHandler = new DebugInvocationHandler(realObject);
+        // 创建代理对象
+        Animal proxy = (Animal)Proxy.newProxyInstance(Cat.class.getClassLoader(),
+               Cat.class.getInterfaces(),
+                debugInvocationHandler);
+        // 使用代理对象的方法
+        proxy.speak();
     }
 }
---
+++ java-basic/src/main/java/com/demnca/java/basic/proxy/vo/Animal.java
@@ -0,0 +1,5 @@
+package com.demnca.java.basic.proxy.vo;
+
+public interface Animal {
+    public void speak();
+}
